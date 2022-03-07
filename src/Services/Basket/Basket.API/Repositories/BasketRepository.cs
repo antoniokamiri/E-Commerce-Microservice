a08c81e2b934cc0e3ee48d50d264b1ec37422f86
Antonio Kamiri <antoniokamiri@gmail.com>
2022-03-07 11:00:14 +0300

    Basket Repository

---
+++ src/Services/Basket/Basket.API/Repositories/BasketRepository.cs
@@ -0,0 +1,48 @@
+﻿using Basket.API.Entities;
+using Microsoft.Extensions.Caching.Distributed;
+using Newtonsoft.Json;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Basket.API.Repositories
+{
+    public class BasketRepository : IBasketRepository
+    {
+        private readonly IDistributedCache _redisCache;
+
+        public BasketRepository(IDistributedCache redisCache)
+        {
+            _redisCache = redisCache ?? throw new ArgumentNullException(nameof(redisCache));
+        }
+
+        public async Task DeleteBasketAsync(string username)
+        {
+            await _redisCache.RemoveAsync(username);
+        }
+
+        public async Task<ShoppingCart> GetBasketAsync(string username, CancellationToken cancellationToken)
+        {
+            try 
+            {
+                var basket = await _redisCache.GetStringAsync(username);
+                if (String.IsNullOrEmpty(basket)) return null;
+
+                return JsonConvert.DeserializeObject<ShoppingCart>(basket);
+            }
+            catch (OperationCanceledException ex) // includes TaskCanceledException
+            {
+                Console.WriteLine("Your submission was canceled." , ex);
+                return null;
+            }
+
+        }
+
+        public async Task<ShoppingCart> UpdateBasketAsync(ShoppingCart basket, CancellationToken cancellationToken)
+        {
+            await _redisCache.SetStringAsync(basket.Username, JsonConvert.SerializeObject(basket));
+
+            return await GetBasketAsync(basket.Username, cancellationToken);
+        }
+    }
+}
---
+++ src/Services/Basket/Basket.API/Repositories/IBasketRepository.cs
@@ -0,0 +1,13 @@
+﻿using Basket.API.Entities;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Basket.API.Repositories
+{
+    public interface IBasketRepository
+    {
+        Task<ShoppingCart> GetBasketAsync(string username, CancellationToken cancellationToken);
+        Task<ShoppingCart> UpdateBasketAsync(ShoppingCart basket, CancellationToken cancellationToken);
+        Task DeleteBasketAsync (string username);
+    }
+}
