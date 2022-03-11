61be046a947cdf85b5ba2d736343d50188d87d99
Antonio Kamiri <antoniokamiri@gmail.com>
2022-03-11 12:05:03 +0300

    GRPC Client Implementation

---
+++ src/Services/Basket/Basket.API/GrpcService/DiscountGrpcService.cs
@@ -0,0 +1,23 @@
+﻿using Discount.Grpc.Protos;
+using System;
+using System.Threading.Tasks;
+
+namespace Basket.API.GrpcService
+{
+    public class DiscountGrpcService : IDiscountGrpcService
+    {
+        private readonly DiscountProtoService.DiscountProtoServiceClient _discountProtoService;
+
+        public DiscountGrpcService(DiscountProtoService.DiscountProtoServiceClient discountProtoService)
+        {
+            _discountProtoService = discountProtoService ?? throw new ArgumentNullException(nameof(discountProtoService));
+        }
+
+        public async Task<CouponModel> GetDiscountAsync(string productName)
+        {
+            var discountRequest = new GetDiscountRequest { ProductName = productName };
+            var response = await _discountProtoService.GetDiscountAsync(discountRequest);
+            return response;
+        }
+    }
+}
---
+++ src/Services/Basket/Basket.API/GrpcService/IDiscountGrpcService.cs
@@ -0,0 +1,10 @@
+﻿using Discount.Grpc.Protos;
+using System.Threading.Tasks;
+
+namespace Basket.API.GrpcService
+{
+    public interface IDiscountGrpcService
+    {
+        Task<CouponModel> GetDiscountAsync(string productName);
+    }
+}
