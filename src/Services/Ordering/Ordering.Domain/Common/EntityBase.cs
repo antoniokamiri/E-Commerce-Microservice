5addbac4656c52d2ca59c35567799d4314a51e3c
Antonio Kamiri <antoniokamiri@gmail.com>
2022-03-14 11:52:29 +0300

    Common Entities

---
+++ src/Services/Ordering/Ordering.Domain/Common/EntityBase.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Domain.Common
+{
+    public abstract class EntityBase
+    {
+        public string Id { get; protected set; } = Guid.NewGuid().ToString();
+        public string CreatedBy { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public string LastModifiedBy { get; set; }
+        public DateTime? LastModifiedDate { get; set; }
+
+    }
+}
