0473898e18b78a0250a29ad0dea111e13f5a28f2
Antonio Kamiri <antoniokamiri@gmail.com>
2022-03-16 11:07:07 +0300

    Order Querys

---
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Queries/GetOrderList/GetOrderListQuery.cs
@@ -0,0 +1,43 @@
+﻿using AutoMapper;
+using MediatR;
+using Ordering.Application.Contracts.Persistence;
+using Ordering.Application.Features.Orders.Queries.DTos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Ordering.Application.Features.Orders.Queries.GetOrderList
+{
+    public class GetOrderListQuery : IRequest<List<OrdersVM>>
+    {
+        public string UserName { get; set; }
+
+        public GetOrderListQuery(string userName)
+        {
+            UserName = userName ?? throw new ArgumentNullException(nameof(userName));
+        }
+    }
+
+    public class GetOrderListQueryHandle : IRequestHandler<GetOrderListQuery, List<OrdersVM>>
+    {
+        private readonly IOrderRepository _orderRepository;
+        private readonly IMapper _mapper;
+
+        public GetOrderListQueryHandle(IOrderRepository orderRepository, IMapper mapper)
+        {
+            _orderRepository = orderRepository ?? throw new ArgumentNullException(nameof(orderRepository));
+            _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+        }
+
+        public async Task<List<OrdersVM>> Handle(GetOrderListQuery request, CancellationToken cancellationToken)
+        {
+            var orderList = await _orderRepository.GetOrderByUserNameAsync(request.UserName);
+
+            return _mapper.Map<List<OrdersVM>>(orderList);
+        }
+    }
+}
+
