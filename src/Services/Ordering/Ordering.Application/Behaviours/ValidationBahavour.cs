b36cdaf208cc9e6a3e4fd99c5a4f4ab7a5fbe574
Antonio Kamiri <antoniokamiri@gmail.com>
2022-03-16 19:17:39 +0300

    UnhandledExceptionBehaviour and ValidationBahavour

---
+++ src/Services/Ordering/Ordering.Application/Behaviours/ValidationBahavour.cs
@@ -0,0 +1,38 @@
+﻿using FluentValidation;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using ValidationException = Ordering.Application.Exceptions.ValidationException;
+
+namespace Ordering.Application.Behaviours
+{
+    public class ValidationBahavour<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse> where TRequest : IRequest<TResponse>
+    {
+        private readonly IEnumerable<IValidator<TRequest>> _validators; //this injection will take all validation classes implemented.
+
+        public ValidationBahavour(IEnumerable<IValidator<TRequest>> validators)
+        {
+            _validators = validators ?? throw new ArgumentNullException(nameof(validators));
+        }
+
+        public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
+        {
+            if(_validators.Any())
+            {
+                var context = new ValidationContext<TRequest>(request); // context of validators
+
+                var validationResult = await Task.WhenAll(_validators.Select(x => x.ValidateAsync(context, cancellationToken)));  // take all validation and perform the fluent validation.
+
+                var failure = validationResult.SelectMany(r => r.Errors).Where(e => e != null).ToList(); // selects the validation failures that have occured
+
+                if(failure.Count != 0) throw new ValidationException(failure); // if errors are their. It throw error using the custom exception
+
+            }
+            return await next();
+        }
+    }
+}
