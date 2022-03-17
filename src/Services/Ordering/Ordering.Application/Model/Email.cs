7c5f75d974bcfe214c464a2fb70c97b1e9ec3724
Antonio Kamiri <antoniokamiri@gmail.com>
2022-03-17 14:08:05 +0300

    Email Code Refactor

--- src/Services/Ordering/Ordering.Application/Model/Email.cs
+++ src/Services/Ordering/Ordering.Application/Model/Email.cs
@@ -11,5 +11,7 @@ namespace Ordering.Application.Model
         public string To { get; set; }
         public string Subject { get; set; }
         public string Body { get; set; }
+
+        public string ToName { get; set; }
     }
 }
