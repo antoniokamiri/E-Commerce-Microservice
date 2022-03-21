0b4f3332d9ed3e4b1f9e708e428bf7c7821c1e7f
Antonio Kamiri <antoniokamiri@gmail.com>
2022-03-21 17:56:48 +0300

    Basket CheckOut Event for rabbitmq

---
+++ src/BuildingBlocks/EventBus.Messages/Events/IntegrationBaseEvent.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventBus.Messages.Events
+{
+    /***
+     * Every rabbitmq message will be inherited from this intergration base event 
+     * including common properities from all events.
+    ***/
+
+    public class IntegrationBaseEvent
+    {
+
+        public IntegrationBaseEvent()
+        {
+            Id = Guid.NewGuid().ToString();
+            CreationDate = DateTime.UtcNow;
+        }
+
+        public IntegrationBaseEvent(string id, DateTime creationdate)
+        {
+            Id=id;
+            CreationDate=creationdate;
+        }
+        public string Id { get; private set; } = Guid.NewGuid().ToString();
+        public DateTime CreationDate { get; private set; }
+    }
+}
