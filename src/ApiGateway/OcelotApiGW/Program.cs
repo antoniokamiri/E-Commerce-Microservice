7a33ee922e0d4c761b3422c45743f459e4e9b276
Antonio Kamiri <antoniokamiri@gmail.com>
2022-03-24 08:20:00 +0300

    Setting up console logs

---
+++ src/ApiGateway/OcelotApiGW/Program.cs
@@ -0,0 +1,33 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OcelotApiGW
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                })
+                .ConfigureLogging((hostingContext, loggingBuilder) => 
+                { 
+                    loggingBuilder.AddConfiguration(hostingContext.Configuration.GetSection("Logging")); // Check logging operation under appsetting.
+                    loggingBuilder.AddConsole(); // provide logs to the console window. 
+                    loggingBuilder.AddDebug(); // enable you to see debug window                                 
+                
+                });
+    }
+}
