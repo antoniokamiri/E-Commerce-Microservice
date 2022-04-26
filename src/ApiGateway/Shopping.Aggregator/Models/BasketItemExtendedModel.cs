c19d9dfb9e89d28f3d53d5f1a45cc73e5b82b14e
Antonio Kamiri <antoniokamiri@gmail.com>
2022-04-26 14:00:07 +0300

    Extended Basket Model

---
+++ src/ApiGateway/Shopping.Aggregator/Models/BasketItemExtendedModel.cs
@@ -0,0 +1,18 @@
+﻿namespace Shopping.Aggregator.Models
+{
+    public class BasketItemExtendedModel
+    {
+        public int Quatity { get; set; }
+        public string Color { get; set; }
+        public decimal Price { get; set; }
+        public string ProductID { get; set; }
+        public string ProductName { get; set; }
+
+        // product related category fileds 
+
+        public string Category { get; set; }
+        public string Summary { get; set; }
+        public string Description { get; set; }
+        public string ImageFile { get; set; }
+    }
+}
